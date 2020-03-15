99bac9fa5e5a01be202991d2b9859ea9f403ab61
michael Zuniga <michaelzunigaa@gmail.com>
2020-03-15 10:02:49 -0700

    first-commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var questionArr = [];
+var score = [];
+
+
+
+
+
+
+var answerA = document.getElementById("choice-1");
+var answerB = document.getElementById("choice-2");
+var answerC = document.getElementById("choice-3");
+var answerD = document.getElementById("choice-4");
+
+var buttonA = document.getElementById("btn-a");
+var buttonB = document.getElementById("btn-b");
+var buttonC = document.getElementById("btn-c");
+var buttonD = document.getElementById("btn-d");
+
+buttonA.addEventListener("click" , function () {
+    console.log("Hi!");
+    
+});
+
+buttonB.addEventListener("click", buttonB );
+buttonC.addEventListener("click" , buttonC);
+buttonD.addEventListener("click", buttonD );
+
+
+
+
+
+
+function getQuestions() {
+    
+}
+
+function displalQuestion() {
+     
+    
+}
+
+function showAnswers() {
+    
+}
+
+function trackScore() {
+    
+}
+
+
+
+
+
